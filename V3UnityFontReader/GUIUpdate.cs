017fd926401c30bc0c959375e9fcdf84e50132f0
IndacoSub <67112008+IndacoSub@users.noreply.github.com>
2022-06-11 18:04:16 +0200

    Divide in multiple files

---
+++ V3UnityFontReader/GUIUpdate.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V3UnityFontReader
+{
+    public partial class Form1
+    {
+        private void PaintRectangle()
+        {
+            PictureBoxImage.Image = new Bitmap(png_fn);
+            PictureBoxImage.Refresh();
+            // Y is inverted and does not account for the character itself
+
+            rectangle = new Rectangle(rect.m_X, InterpretY(rect.m_Y), rect.m_Width, rect.m_Height);
+
+            //rectangle = new Rectangle(font.m_UsedGlyphRects[cur_index].m_X, InterpretY2(font.m_UsedGlyphRects[cur_index].m_Y), font.m_UsedGlyphRects[cur_index].m_Width, font.m_UsedGlyphRects[cur_index].m_Height);
+            using (Graphics gr = Graphics.FromImage(PictureBoxImage.Image))
+            {
+                using (Pen pen = new Pen(Color.Red, 1))
+                {
+                    gr.DrawRectangle(pen, rectangle);
+                }
+            }
+
+            PictureBoxImage.Refresh();
+        }
+
+        private void PaintRectangleUsed()
+        {
+            PictureBoxImage.Image = new Bitmap(png_fn);
+            PictureBoxImage.Refresh();
+
+            //Debug.WriteLine("Width: " + font.m_UsedGlyphRects[cur_index].m_Width + ", Height: " + font.m_UsedGlyphRects[cur_index].m_Height);
+
+            // Y is inverted and does not account for the character itself
+            rectangle = new Rectangle(font.m_UsedGlyphRects[cur_index].m_X,
+                InterpretY2(font.m_UsedGlyphRects[cur_index].m_Y),
+                font.m_UsedGlyphRects[cur_index].m_Width,
+                font.m_UsedGlyphRects[cur_index].m_Height);
+
+            using (Graphics gr = Graphics.FromImage(PictureBoxImage.Image))
+            {
+                using (Pen pen = new Pen(Color.Red, 1))
+                {
+                    gr.DrawRectangle(pen, rectangle);
+                }
+            }
+
+            PictureBoxImage.Refresh();
+        }
+
+        private void UpdateTextboxString()
+        {
+            int index = font.m_GlyphTable[cur_index].m_Index;
+            TMPCharacter character = GetCharacterFromIndex(index);
+            if (character == new TMPCharacter())
+            {
+                return;
+            }
+
+            char c = (char)character.m_Unicode;
+            int x = rect.m_X;
+            int y = rect.m_Y;
+            InfoTextbox.Text =
+                "m_Index: " +
+                index +
+                ", character: \"" +
+                c +
+                "\" (" +
+                character.m_Unicode +
+                ")" +
+                ", X: " +
+                x +
+                ", Y: " +
+                y +
+                ", InterpretedY: " +
+                // Y is inverted and does not account for the character itself
+                InterpretY(y) +
+                ", cW: " +
+                rect.m_Width +
+                ", cH: " +
+                rect.m_Height
+                ;
+            //InfoTextbox.Text = "Index: " + cur_index + ", inside is: " + (char)WhatIsInsideGlyphRect(font.m_UsedGlyphRects[cur_index]).m_Unicode + " (" + WhatIsInsideGlyphRect(font.m_UsedGlyphRects[cur_index]).m_Unicode + "), X: " + font.m_UsedGlyphRects[cur_index].m_X + ", Y: " + InterpretY2(font.m_UsedGlyphRects[cur_index].m_Y);
+            InfoTextbox.Update();
+            InfoTextbox.Refresh();
+
+            LabelTableY.Text = "Table Rect Y (" + c + ")";
+            LabelUsedY.Text = "Used Glyph Y (" + c + ")";
+        }
+
+        private void IncreaseIndex()
+        {
+            if (!loaded_png || !loaded_txt)
+            {
+                return;
+            }
+
+            if (cur_index + 1 >= font.m_CharacterTable.Count)
+            {
+                cur_index = font.m_CharacterTable.Count - 1;
+                return;
+            }
+
+            cur_index++;
+            //Debug.WriteLine("cur_index: " + cur_index + ", gTable: " + font.m_GlyphTable.Count + ", cTable: " + font.m_CharacterTable.Count);
+            rect = font.m_GlyphTable[cur_index].m_GlyphRect;
+        }
+
+        private void DecreaseIndex()
+        {
+            if (!loaded_png || !loaded_txt)
+            {
+                return;
+            }
+
+            if (cur_index <= 0)
+            {
+                cur_index = 0;
+                return;
+            }
+
+            cur_index--;
+            rect = font.m_GlyphTable[cur_index].m_GlyphRect;
+        }
+    }
+}
---
+++ V3UnityFontReader/Operations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V3UnityFontReader
+{
+    public partial class Form1
+    {
+        private int InterpretY(int y)
+        {
+            // Used to read "partial glyphs" a.k.a. letters/numbers/symbols
+            int ret = PictureBoxImage.Image.Size.Height - y - rect.m_Height;
+            return ret < 0 ? 0 : ret;
+        }
+
+        private int InterpretY2(int y)
+        {
+            // Used to read *used* "full glyphs" a.k.a. letters/numbers/symbols PLUS their surrounding area (used in colored CLT)
+            int ret = PictureBoxImage.Image.Size.Height - y - font.m_UsedGlyphRects[cur_index].m_Height;
+            return ret < 0 ? 0 : ret;
+        }
+
+        private int InterpretY3(int y)
+        {
+            // Used to read *free* glyphs (dumbest feature ever)
+            int ret = PictureBoxImage.Image.Size.Height - y - font.m_FreeGlyphRects[cur_index].m_Height;
+            return ret < 0 ? 0 : ret;
+        }
+
+        private int NormalizeY(int y)
+        {
+            return Math.Abs(y - PictureBoxImage.Image.Size.Height + rect.m_Height);
+        }
+
+        private int NormalizeY2(int y)
+        {
+            return Math.Abs(y - PictureBoxImage.Image.Size.Height + font.m_UsedGlyphRects[cur_index].m_Height);
+        }
+
+        private int NormalizeY3(int y)
+        {
+            return Math.Abs(y - PictureBoxImage.Image.Size.Height + font.m_FreeGlyphRects[cur_index].m_Height);
+        }
+
+        // Remove spaces (https://stackoverflow.com/questions/6219454/efficient-way-to-remove-all-whitespace-from-string)
+        public static string RemoveWhitespace(string input)
+        {
+            return new string(input.ToCharArray()
+                .Where(c => !char.IsWhiteSpace(c))
+                .ToArray());
+        }
+    }
+}
